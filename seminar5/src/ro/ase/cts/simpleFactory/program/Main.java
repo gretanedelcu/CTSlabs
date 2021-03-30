e61dce44ab49dc12402f96551ffa8a83b51f3b16
greta <greta@DESKTOP-M7A9QT9>
2021-03-30 23:04:56 +0300

    seminar 5 v2

--- seminar5/src/ro/ase/cts/simpleFactory/program/Main.java
+++ seminar5/src/ro/ase/cts/simpleFactory/program/Main.java
@@ -8,18 +8,31 @@ import ro.ase.cts.simpleFactory.clase.TipPersonal;
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-		// TODO Auto-generated method stub
-		FactoryPersonal factoryPersonal= new FactoryPersonal();
-		PersonalSpital medic=factoryPersonal.getPersonal(TipPersonal.MEDIC,"Ana");
-		PersonalSpital asistent=factoryPersonal.getPersonal(TipPersonal.ASISTENT,"Catalin");
+		//=======DESIGN PATTERNS========
+		//======= SIMPLE FACTORY ======
+		// simple factory folosim cand avem o familie de clase (medic, brancardier, asistent)
 		
-		System.out.println(medic.toString());
-		System.out.println(asistent.toString());
-		}catch(Exception e){
+		// problema cu Spitalul, ex2
+		
+		//putem sa avem atribute -> facem clasa abstracta (nu interfata)
+		// clasa abstracta: PersonalSpital
+		//Medic, Brancardier, Asistent <- familia de obiecte
+		//TipPersonal - enum cu cele 3
+		
+		//clasa fabrica: FactoryPersonal
+		//getPersonal(TipPersonal):PersonalSpital
+		
+		try {
+			FactoryPersonal factory=new FactoryPersonal();
+			PersonalSpital medic=factory.getPersonal(TipPersonal.MEDIC, "Ana" );
+			PersonalSpital asistent=factory.getPersonal(TipPersonal.ASISTENT, "Ion");
+			
+			System.out.println(medic.toString());
+			System.out.println(asistent.toString());
+		
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 
 }
